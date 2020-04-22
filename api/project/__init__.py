a7aa0f963cf0eedf8edc91eb8fc18aa86773cec2
Martin David Valentino <martin.valentino@cultureamp.com>
2020-04-22 15:35:10 +1000

    Update project with the latest dependency packages

--- api/project/__init__.py
+++ api/project/__init__.py
@@ -2,13 +2,20 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 db = SQLAlchemy()
 cors = CORS()
 
 
 def create_app(script_info=None):
+    # This is a workaround for Alpine Linux (musl libc) quirk:
+    # https://github.com/docker-library/python/issues/211
+    import threading
+    threading.stack_size(2 * 1024 * 1024)
+
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app)
 
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
@@ -21,6 +28,6 @@ def create_app(script_info=None):
 
     @app.shell_context_processor
     def ctx():
-        return {'app': app, 'db': db }
-    
+        return {'app': app, 'db': db}
+
     return app
--- api/project/db/create.sql
+++ api/project/db/create.sql
@@ -1,3 +1,2 @@
-CREATE DATABASE managertools_prod;
-CREATE DATABASE managertools_dev;
-CREATE DATABASE managertools_test;
+CREATE DATABASE db_dev;
+CREATE DATABASE db_test;
